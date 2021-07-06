fafa5d557668bc29d341fd8ff142102417384930
paxuy <luap.schillinger@gmail.com>
2021-07-06 19:01:14 +0200

    can't remember

--- js/paxuytype.js
+++ js/paxuytype.js
@@ -1,40 +1,26 @@
 //---------Programm---------
 
 let jsonObj = loadJSON("../paxuytype/texts/texts.json");
-let speedtext = jsonObj[0];
+let speedtext = jsonObj[2];
 
-/*
-let correct = document.createElement('span');
-correct.id = "correct";
-document.getElementById("speedtext").append(correct);
-correct = document.getElementById("correct");
+document.getElementById("inp").addEventListener("input", updateText);
 
-let coming = document.createElement('span');
-coming.id = "coming";
-document.getElementById("speedtext").append(coming);
-
-coming = document.getElementById("coming");
-*/
-/*
-speedtext.split("").forEach(letter => {
-    let spa = document.createElement('span');
-        spa.innerHTML = letter;
-    document.getElementById("speedtext").append(spa);
-});
-*/
-var spans = document.getElementById("speedtext").childNodes;
-
-
-//document.getElementById("coming").innerHTML = speedtext;
-
-
-
-/*
-document.getElementById("inp").addEventListener("input", trigger);
-function trigger() {
-    document.getElementById("inp").value += "got it";
+document.getElementById("speedtext").addEventListener("click", focusInput)
+function focusInput() {
+    document.getElementById("inp").focus();
 }
-*/
+
+document.getElementById("inp").addEventListener('paste', e => e.preventDefault());
+document.getElementById("inp").addEventListener('drop', e => e.preventDefault());
+
+
+document.getElementById("inp").addEventListener("input", startTimer);
+function startTimer() {
+    document.getElementById("inp").removeEventListener("input", startTimer);
+    runTimer = true;
+    setTimeout(timer, 1000);
+
+}
 
 let time = 0;
 let runTimer = false;
@@ -57,37 +43,10 @@ function timer() {
     
 }
 
-
-
-
-
-
-//document.getElementById("inp").addEventListener("input", updateOutput);
-//document.getElementById("inp").addEventListener("input", checkInput);
-document.getElementById("inp").addEventListener("input", updateText);
-document.getElementById("speedtext").addEventListener("click", focusInput)
-document.getElementById("inp").addEventListener('paste', e => e.preventDefault());
-document.getElementById("inp").addEventListener('drop', e => e.preventDefault());
-//document.getElementById("inp").addEventListener("input", reset);
-
-
-document.getElementById("inp").addEventListener("input", startTimer);
-function startTimer() {
-    document.getElementById("inp").removeEventListener("input", startTimer);
-    runTimer = true;
-    setTimeout(timer, 1000);
-
-}
-
 function stopTimer() {
     runTimer = false;
 }
 
-
-
-function focusInput() {
-    document.getElementById("inp").focus();
-}
 
 let words = speedtext.split(" ");
 let currentWord = 0;
@@ -106,30 +65,12 @@ for (let i = 0; i < words.length; i++) {
     document.getElementById("speedtext").append(wordSpan);
 }
 
-
 let letters = speedtext;
 let currentLetter = 0;
-var text = document.getElementById("speedtext");
-let wrongExist = false;
+let text = document.getElementById("speedtext");
+
 document.getElementById("inp").focus();
 document.getElementById("inp").value = " ";
-function nextWord() {
-    currentLetter += words[currentWord].length
-}
-
-
-
-
-/*
-function cursor() {
-    for (let i = text.childNodes[currentWord].childElementCount-1; i >= 0; i--) {
-        if (text.childNodes[currentWord].childNodes[i].id !== "") {
-            text.childNodes[currentWord].childNodes[i].id = "";
-            text.childNodes[currentWord].childNodes[i+1].id = "current";
-        }
-        
-    }
-}*/
 
 text.childNodes[0].childNodes[0].id = "current";
 
@@ -137,10 +78,9 @@ function updateText() {
 
     document.getElementById("current").id = "";
 
-
     let currentInput = getInputvalue();
 
-
+    //Space
     if (currentInput === "  ") {
         let wordCorrect = true;
         for (let i = 0; i < text.childNodes[currentWord].childElementCount; i++) {
@@ -161,6 +101,7 @@ function updateText() {
         text.childNodes[currentWord].childNodes[currentLetter].id = "current";
         document.getElementById("inp").value = " ";
     }
+
     //Richtig
     else if (currentInput === " " + words[currentWord][currentLetter]) {
 
@@ -168,12 +109,7 @@ function updateText() {
         currentLetter++;
         text.childNodes[currentWord].childNodes[currentLetter].id = "current";
         document.getElementById("inp").value = " ";
-    }/*
-    else if(currentInput === " "){
-        document.getElementById("currentWrong").outerHTML = "";
-        wrongExist = false;
-
-    }*/
+    }
 
     //Löschen
     else if (currentInput === "") {
@@ -189,7 +125,7 @@ function updateText() {
                 text.childNodes[currentWord].childNodes[text.childNodes[currentWord].childElementCount - 2].outerHTML = "";
                 text.childNodes[currentWord].childNodes[text.childNodes[currentWord].childElementCount - 1].id = "current";
             }
-            else if (!text.childNodes[currentWord].childNodes[text.childNodes[currentWord].childElementCount - 2].hasAttribute("class") && text.childNodes[currentWord].childNodes[text.childNodes[currentWord].childElementCount - 2].className === "") {
+            else if (!text.childNodes[currentWord].childNodes[text.childNodes[currentWord].childElementCount - 2].hasAttribute("class") || text.childNodes[currentWord].childNodes[text.childNodes[currentWord].childElementCount - 2].className === "") {
                 let breakFor = false;
                 for (let y = currentWord; y >= 0; y--) {
                     for (let i = text.childNodes[y].childElementCount-1; i >= 0; i--) {
@@ -228,26 +164,9 @@ function updateText() {
             text.childNodes[currentWord].childNodes[currentLetter].className = "";
             text.childNodes[currentWord].childNodes[currentLetter].id = "current";
         }
-
-        /*
-        else{
-            if (currentLetter === 0) {
-            currentWord--;
-            currentLetter = words[currentWord].length-1;
-                
-            }
-            else{
-                currentLetter--;
-            }
-            if (text.childNodes[currentWord].childNodes[currentLetter].className === "wrong") {
-                text.childNodes[currentWord].childNodes[currentLetter].innerText = words[currentWord][currentLetter];
-            }
-            text.childNodes[currentWord].childNodes[currentLetter].className = "";
-            text.childNodes[currentWord].childNodes[currentLetter].id = "current";
-        }
-        */
         document.getElementById("inp").value = " ";
     }
+
     //Wrong
     else {
         if (currentLetter < words[currentWord].length) {
@@ -262,76 +181,25 @@ function updateText() {
             extraWrong.className = "extraWrong";
             text.childNodes[currentWord].insertBefore(extraWrong, text.childNodes[currentWord].childNodes[text.childNodes[currentWord].childElementCount - 1]);
             text.childNodes[currentWord].childNodes[text.childNodes[currentWord].childElementCount - 1].id = "current";
-
-            /*
-                            text.childNodes[currentWord].childNodes[currentLetter].innerText = currentInput.charAt(1);
-                            text.childNodes[currentWord].childNodes[currentLetter].classList.add('wrong');
-                            text.childNodes[currentWord].childNodes[currentLetter].id = "current";
-            */
-            /*
-            let extraWrong = document.createElement("span");
-            extraWrong.innerText = currentInput.charAt(1);
-            extraWrong.className = "extraWrong";
-            text.childNodes[currentWord].append(extraWrong);
-            */
-
-
         }
-
         document.getElementById("inp").value = " ";
-
-
-        /*
-        if (!wrongExist) {
-            let wrong = document.createElement('span');
-            wrong.id = "currentWrong";
-            text.insertBefore(wrong, text.children[currentLetter]);
-            wrongExist = true;
-        }
-        
-        //Skip wrong characters with space
-        if (currentInput.charAt(currentInput.length - 1) === " ") {
-            document.getElementById("speedtext").childNodes[currentLetter].id = "";
-            document.getElementById("speedtext").childNodes[currentLetter].classList.add('wrong');
-            currentLetter++;
-            document.getElementById("inp").value = " ";
-            wrongExist = false;
-        }//Show wrong characters
-        else{
-            document.getElementById("currentWrong").innerText = currentInput.slice(1);
-        }
-        */
-
     }
 
-
+    //Finish
     if (currentWord === words.length-1 && currentLetter === words[words.length-1].length) {
         finish();
     }
 
+    //Debug
+    /*
     document.getElementById("debug").innerText = `Current Word-Letter(${currentWord}-${currentLetter}): -${words[currentWord][currentLetter]}-
     Current Input: -${currentInput}-`;
-
-    /*
-        if (getInputvalue() === letters[currentLetter]) {
-            
-            
-    
-            correct.innerText += letters[currentLetter];
-            if (letters[currentLetter] !== " ") {
-                coming.innerText = coming.innerText.slice(1);
-            }
-            else{
-                coming.innerText = coming.innerText;
-            }
-            currentLetter++;
-            document.getElementById("inp").value = "";
-    
-        }
-        document.getElementById("debug").innerText = `n=${currentLetter}  Letter: -${letters[currentLetter]}-`;
-        */
+    */
 }
 
+function typing() {
+    document.querySelectorAll("body > p:not(.speedtext)").style
+}
 
 function finish() {
     stopTimer();
@@ -344,20 +212,6 @@ function finish() {
     let accuracy;
     document.getElementById("result").innerText = ``;
 }
-
-//document.getElementById("inp").addEventListener("keydown", ifDelete);
-function ifDelete(event) {
-    var KeyID = event.keyCode;
-    switch (KeyID) {
-        case 8: case 46:
-
-            break;
-        default:
-            break;
-    }
-}
-
-
 
 //---------Functions---------
 
@@ -391,23 +245,4 @@ function reset() {
     if (getInputvalue().charAt(getInputvalue().length - 1) === " ") {
         document.getElementById("inp").value = "";
     }
-}
-
-
-
-setCaretPos(document.getElementById('speedtext'), 10); // pure JS
- 
-// Definition
-function setCaretPos(domElem, pos) {
-  if(domElem.setSelectionRange) {
-    domElem.focus();
-    domElem.setSelectionRange(pos, pos);
-  }
-  else if (domElem.createTextRange) {
-    var range = domElem.createTextRange();
-    range.collapse(true);
-    range.moveEnd('character', pos);
-    range.moveStart('character', pos);
-    range.select();
-  }
 }
