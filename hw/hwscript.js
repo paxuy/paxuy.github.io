93bd3a713ae2b482b9f8928583284c1e9c908df8
uystq <uystq@student.kit.edu>
2024-04-17 03:46:23 +0200

    hw update

--- hw/hwscript.js
+++ hw/hwscript.js
@@ -1,4 +1,7 @@
-var encrptedMessage = "U2FsdGVkX19bJE+J7TkRSdK2tKA/+WMtlOmvEETrX4Cfee9beGHNna50eXstVcBXp9AnfP6V4f089zRRvfSsXiN6vEz8zuQosQWXbmMYH7aPx1qgv9kh8JI2tYmkbRgmKl8Ob21lmja1tFOhEGTag9l9ZB8aQNYZW5xvo/t14nzMUOFnOtb5zT5XE19yNW1rXhQbDc+jBQe+1BfutLEtP8XnD/MuJQ/1AA56KJgsu6+LJu6rckm+3hWdv18+ZrWoCrWLtkPeda3NYy/paSprTwEvvN8KvyjkOzNs4110bHhNUtSFRPyD5ctc4s0B9RiFkr3eV3iHwlO6SJRgYaB3myWfJ+kaZ233nCYPTjSy0lbWKw2WgTEUosvDkTKPEoouj0oDpNQqEr5Z259xDHqeETZGg4xXPOu6SrTQpkDTNmVBNnHAD4mvVCTO2opagXPY6aay6VYh0n6thpTR+8dvd5poNUyoTQcKVwb78Bco5raYTcqHNaW96h3w7meaZU43KU2IPhJTJA1NvvTDMu/FPFyI9zGFpSovQRRlQZI+cAU98mcR0Z9ai0QGSWBWz0JOD3J4Kaq9vLj31EE9Ug0wtBxb/Hu3QmC3aj6ve0oZUcVHuVDqFRheLSIm8OzUG2A2dBLRpqo0OBok/8tL7QEN0eOfvB7bWf26CKOO5SDe5Uy/AiYbltAYP6OWewNfaezE9yLXdOW+Z/hOx2qCvgr85rrPxF2W6TWOpvIMj35bV9CD2FcRzZqSO83f8Lh7H/tHR6/Ke2UUTnBPjF7C3Uhwtkiqf3fBfyjbsM7i3areQz4="
+var encrptedMessage1 = "U2FsdGVkX19Q4aQ7N1GuM9N0XSMGC11Hzs/W9OknaxRX6l2k6Q0L/oaj3c/griQjIto6tEgIPKhLew+Ak0pIPJnK7IEt9s7NexhBbErBlpwuGDEsER5txxecNfNfamO5wCRGJUbPRe6Bnd1OcpYEvjEG8cv0ff2q4q6+Ykg+gicajFv2BLMYbT4M5eSszwtFYRNCNFDeSjb5/5wUbcBvleLGqfejF+fGeR12gPDyN5bINDO5CsRyyWiXUUOkeaidOP5bDYJJIWgkcDTApxtHXnby7sJM44l2msobIYkWmnzMrdC6Z6YYyi+1QfKtGCQNyVmuyOerYOhlEsHC58nvHjZ0PJms0tsVKjBnG3DlhLxKeu0UJhT0ksoWugh8JQeoPhsh96017Z4sGjNWD2y0On+aLKuLzItUv1o0NuVerOXuvlMFmskTMzuX5MfsIUrXJCrtWgqDjNeItZq9gp+MS9kArmlGpSMWjzguLuysrUKl1NbRh1iAxoR46NqFBpi50Kb1H7H4MEDJhJibCES6XhsRVzWP3i7q5VbUNoogfhWW67fdcv2gKwhrZEMGeEWBDNiavaAIlbTKjW2qWrXNM/F7nNa1L04d73uCrqgPhrlz2J2P4rRyXVixwitXPss7CXAJgHe/zHKx9zct9qCvZ5iGM4z1UTWn/nBh8QZdwO0epFD8R9GdijTt0SOE3P1DyGTpOJ/i533aVHjt8yt30SXsAJ/WFYRPTuIDCAqv641iz9RX5ew0MWSeUDy5CJO3xnQn//C0n3c2K3dy2TB3X6hZA1cvRGEdrx9f/aAZ/NJQmTy63hhvyTruIuvaf5MkkLjq3J6wdbSSJfGSwkrFycbnn+2MuW9u+Nlicvz9Y/dg31HFJ5vu6opzHlm0xas59ulPLKkz0yLK9TcTeduDtQmih4CyuAQSJKErD2y++R/s9NtRy464i4A+ALdvH4yTAZLkJoZS5QORR6+zuCwrYse+a1X7QTRcX81lC9hMdqkVmufjEanbtW93F7QlAESPlWlAZks5UUzzpNtx/JOSCvG6Kl94FQE3qwlYu77mLui8PhVkFYCp2i2qOVAO7sFxkTTkQ8Ob/5F8LT934q+uIOWLmHQwdBhKkm00nrhP+kQgV7b+oQGfG9Hj4ewlEFqY2i0nQt4c3jBKyFmiJNADGS+98WPBf7FZAlYmJYNZHjcenH+xiBS48Z6HEXXQP4pj4t4ZKNrcpwG0CbDX/Ft7+TtVxbLYOogaY4EzdAxzvSW14ZmXTBYvcT1ctEcjoUctoGkkFBWp4sctF7l7DEHquhLuoFOPRbGkxOW6kdEDXVeOITwl2I57TBKSw7hLjXweTFCEwl//CycczlGT4rcdZXfdRDy1WRxLWPiO9K/lWVs=";
+var encrptedMessage2 = "U2FsdGVkX1+HoANrdxvoCK3InHVjnGN16MiM5UxOQ2+SXJ5X6jDdqZPuq3ANfbm0irXStP4PEPpxZ2n58vf/1blcgxVwLaHXkeZmrj4z1khmp7gVuvJw8heXhLkwrb2HV1QY9uwOl5kWzCj+k+hAzaateFqs1R0RcxJEI84wxsUqmQvYWpUFqiBATeMpNpo7dV0aPsIzLtJMcpGXrFy5UO9kKQVOuvauN4xXmSqvreyrzITEZVgJ65IE8XX4GIXlYqJxBq8PXHuI7MBfIehti92ksfLkv6zwBqkW39CbPNCDmtcOEivADx8mPRd441kKMMrg5Xpw7aD2TAAu3IxJRFEU8UpuyUJB5aumaUg+Y8gBi6nC+mNFiFx5Zy7byCieKGtjbmRp8ANVY+tADQeuKYbMbaMAMAe/yw8tZQ7bra7maXzXEADx/TyuF+zucE41i5kv4UXPmsooVJWpTi+yPfVzKH/uBu3UtDByPRf7E6omhgoGy30VGJd5ZYQSv9fQHjkzQ13tqA+w3ey7MmvkLnyCZ1ApE+sNXKvhTdN9lmNoP1KPmywJCwQbMFkAPAhJjheQDVJGgf1TejE4EfAY2MUXKUslW5+0WaQNa8GnxIPL/j7GcvF1RfUbT18YvdQPpUJWm11ys5ShEIhBZFmYl3h+BUS4EJY1ikrlgXU4wvg/zfixMIgc0FrY5fpe/K3YG37Hbqcz8QDgjEjwRxXUVde4y64YkBSHacKq2KVEe+PtkgkV2V1jg7O/kgGMZwpq2mweAD6RYxi8b3dCR9O+PbXz7khJnMgRckpVOp1EczEGF5RRkxffcpvGtSvqLUd/RBO9wboO7Akkj3uHhVmQ79FhZQzHF1/7Qt60j1n9KcuU5+ByavDtP0A0afg4d35mskSP2q6f2bLdAXIMCHMfbFqjehfUMPvs/l+dw3NTgG4/+dhWiSqO1hS+qNGdTlzf1kFASbjgjv9DQ+AZzRpaAFhpnGi5yRL5JqnIgM6pdZkn0j5Zwyz3ACOEcmrggoC8RxsJH2UciJ8PkSlCsJUMtUOWm7vCXKOZQ+Zr9yTCqhLHjCKUeHzkkUfSOVLckgr6eHC+iwPHl57+EMuRMygZPXG+LZA5sonoQyqUrT+2Prvg53HCNjL2xWn+AHymnBop0etEIm5tI7WDlagtXiX/dPpVgo2h2VqzT06sWH66nmKYYgFnMUBzBBYiiWbVgNpXd7adazRm9S1J7TiJnjYnzQliwrf1ETnb5XwGX2Kxjruys08kDLUTgFDXsSsHSst0RTlc7333Nu2MgnQ8M4Uef+UD1+oF8g7zmO1H+Oxkvb7H7FLtB6CiVQ8lF2dkatgID70pzajIr2kf6yYSrc6DENKtnHYp3A77IjgeRMLetc4=";
+var encrptedMessage3 = "U2FsdGVkX1/J5OinByi18hQaGnjWLKwc6IUlFPo+W4tPBTHClQ0ZpGLX6tSMQvyUsGaN5tUZ1RKMSewkimaE8Z6jJ0R6Y6z64Va8Cb2eNbyBvekTPRinoPCwNTH9mQseZ/Zs0jX+tubv52Szx9FEVnBoK/5Db10FrfuNN37lXb5lozVU9sNAjvw7xg0s4eeuhwQ2xvBiNRnc9mlnjAmq/X4aVpc7J4HvhdWd50xDOrjqbyKWpPIML6Mi211vJjB3Jpnlz95fM9Jw22051fT+CUhKTiFrh01mBjThUZnlThr0F8QfGNsf1G0A77wAClMM1yRzEK2F4zQ8gxGxqsq8flrGNPoTwb/HQrYC9KGSmOB2TUc4qXf2W0mInKOftC8DQuxQfrRxUgWHyuPXX4IZGpw6ig7jTbiFGE4sUC2lfWDJ/E4nx/pLiuTtLHvandU6XwFFFUZClpT1AqaviUTrzT9QyuIiUE9mB08w10j35TCCU1hh8ROlWKGyBKRBaS5r94rL3odOdz5FkE+1xS34VLx3+xxEt4Mu+dWnLV822js/2uTuepFo2rBoC4qXoRdPt9LLNRP9xqB1UagaR/z6iXkhAEkSmrl0fdHedhlBU9kE0a9I8/ZDtgCaeITxDscmXPY4hDFemE1C+xHhYulv9ahm21aPfaULw/NTqsdisEHES4VK2sJsg0Sk9DJCX/FoAcFceDKzsNkXmWm5Wkg+vjx/aEp/8PHg2iiEwE0dHIHqalWN6xntyvMNhYjGcrDJgZESYN5NSSP3XRDfX0/Y/LX2II6rA3SqSaeKb/JOaBfTuiWBpwtxHZ1TsufxldBPtNUKxniExU4zHK9WvuB9wfZEYQRE4L32PA5dajOuiJC+YYD8UPf35/sz6rYhKOoKye78iLQNrA1jSeKA22nfb0HOEShEf1J/ZcXS43f1SPTpi5zA6WbL+BeBvHRpeCcq9TXw3CU+FqGXpHWpXC8L2FG8SxStN2y6dfNcje+MTJigxUIIndON6DebU3vnCRWrvbA1bcmeUxB5wrsrEYpc8Yg48nWBMW1TtcMEij5O/MFtKZ5+/336BpsIPSPm4DPoIOl7C5rOvfnWrXYRB9cLMnW7hSh700jLc8flHYDX0w3PyA8bU5UdshCxzXbONPr69HIce7jcrNZzuDFYOrJxaLkbd+3n5ZIRaFr5dSOapZzfMkYFC/68TvYaICeoLnVhx8fnyx8fvg6Y2jPYOy77kH2BFl+fzV2gwSOMnJMgztwTo+a9MKUE3wwckIRTwgB8/cy2J0m2nM42lxkg1W8xt5AJuozVljWbdCDX1hf0O9ohzJUrGuAqHTnIbx34DmE+IOI5UnjuKoLjyWBHBSEE3Xr9e739MJ72b7lpjjL1vpk=";
+var encrptedMessage4 = "U2FsdGVkX18MjN/5LJ902Q4ry3uaQ/U3ob3Gedmxn77EB1IVmBhsp25KJrijrAOZZqS8SopqyNg/0epQ/PE0ii1sbpdQghPPGxKWNYAKaYqxOV/W/GLEWPvTEoI/d08K3DIG7EqWdW3ptgDCSbdOJU0IBqEg49y/xc46zWaogOUy0FyyMbWfp4f78JwFFwH1MqPAK+BqrrUKOewTvdP0IRQI3Jro8Fgs4iJzIWU/7MWuQFHDSXmCw+aj5PBuULp5oj9XGopgpi7zi7Pf29pqHYXQ9QiTlqATPDbX2xq+Df4mAYPhfz/Iq/8dLTtXir7HB0gtsDX5gqmgemN0n6P/WAt6aqXforc8N48i4G942Y7EO4babMgAU7yl2G95dYdsB0kugU9NzuJ8AuNQWKkGwl42+Y0zGq2aMBpYbPupVPwxYoi2MVcs/JuayNaf94/hG7S6TUdHbM4gpBq1LB0zG1SgL9VvrWxcI5q6f1Ab2HxV2Xql1I1DSfcjbVnG0zfGbOtUJl0NXk8MZKUpVUJJ3RCZbb1oiU4FCp3hj3XYeSH0FSNxYvsaOipeKR3w7ZvI5A693sIVSJHQaHLfv42aptAXhiSlxEirWi9y2Amyj87OiCekar6fYBiY63zKTwZJe+YJ2m8L3je5kkBEVBSYB5/rX772QmgjhDyYqd0WfqTVleM918o6RzzEH91sma2ZivjJSXApSMNT1T3/b+MBpN9OYEsJU9uWYC8Va9QeLN1KZuocQOyzf2Co77C1/RBKlZ1J0SvEPXEP4nWDNBQNdH4RjtdnNXiQA9ewuemeWK/Kl+biKrzT4khik6LZ3357fH7iomyhMjI260/lZzMVMGTk21KuXgoO/wshQi0dLsmq0TbWi0MDfUoJwbV8YWBjw6KljtQ9qut7GI1q2pLw/8UlkthzmlfFsCpbEQWzzwc5glsFfAbiw8v4Vx98UpP8D6VMvmL7LE2iCNoiTTbylypnqvrJRXL634WUcQHgJwiP6S/2Q6wuUdpdiGJ4Ur4sXlRtLb0d5KNJndSrnwE/bAzkQBmiriBYs59AS2yeBK2j8IiECo8X9jmCOqkwCIHW8Ku/zH3gMTpiW8g47OqTYwojIsXxXvFQqZN/l/MJlVq8b93oCVinipT+fw1JtxmfYcy75G4P7lV7IkWo901Yk3v0B0ibhf2NrndpyuXO2+enxCHKgXU8LBkmPKV8GQKLytcJ5ThXrksGm14IXzormUHn3BZTP9tqQ7zbQpwOGGg4426bY9GUZMH3F/c3OjNejqpMFB0f0YHZyiXWCvZrnvdCfSWiOqUHi24wfX+Pr6OIBfr80fJll7qEWSr2RjP1tx/G2hS8WaBNIGzrDQ6ATA/SeNTyjbHcQuP0/joDfVs=";
 
 
 function hashString(string) {
@@ -61,10 +64,15 @@ function decryptMessage(ciphertext, key) {
 
 
 window.onload = function () {
-    // document.getElementById('encryptedMessage').innerText = encryptMessage(document.getElementById('message').innerText, hashString('egri'));
+    document.getElementById('encrypted1').innerText = encrptedMessage1;
+    document.getElementById('decrypted1').innerText = encrptedMessage1;
+    document.getElementById('encrypted2').innerText = encrptedMessage2;
+    document.getElementById('decrypted2').innerText = encrptedMessage2;
+    document.getElementById('encrypted3').innerText = encrptedMessage3;
+    document.getElementById('decrypted3').innerText = encrptedMessage3;
+    document.getElementById('encrypted4').innerText = encrptedMessage4;
+    document.getElementById('decrypted4').innerText = encrptedMessage4;
 
-    document.getElementById('encrypted1').innerText = encrptedMessage;
-    document.getElementById('decrypted1').innerText = encrptedMessage;
     // document.getElementById('encrypted2').innerText = encryptMessage(document.getElementById('part2').innerText, key2);
     // document.getElementById('encrypted3').innerText = encryptMessage(document.getElementById('part3').innerText, key3);
     // document.getElementById('encrypted4').innerText = encryptMessage(document.getElementById('part4').innerText, key4);
@@ -98,15 +106,15 @@ function checkAnswer(answer, number) {
         case 1:
             document.getElementById('decrypted1').innerText = decryptMessage(document.getElementById('encrypted1').innerText, hashedAnswer);
             break;
-        // case 2:
-        //     document.getElementById('decrypted2').innerText = decryptMessage(document.getElementById('encrypted2').innerText, hashedAnswer);
-        //     break;
-        // case 3:
-        //     document.getElementById('decrypted3').innerText = decryptMessage(document.getElementById('encrypted3').innerText, hashedAnswer);
-        //     break;
-        // case 4:
-        //     document.getElementById('decrypted4').innerText = decryptMessage(document.getElementById('encrypted4').innerText, hashedAnswer);
-        //     break;
+        case 2:
+            document.getElementById('decrypted2').innerText = decryptMessage(document.getElementById('encrypted2').innerText, hashedAnswer);
+            break;
+        case 3:
+            document.getElementById('decrypted3').innerText = decryptMessage(document.getElementById('encrypted3').innerText, hashedAnswer);
+            break;
+        case 4:
+            document.getElementById('decrypted4').innerText = decryptMessage(document.getElementById('encrypted4').innerText, hashedAnswer);
+            break;
         // case 5:
         //     document.getElementById('decrypted5').innerText = decryptMessage(document.getElementById('encrypted5').innerText, hashedAnswer);
         //     break;
@@ -124,7 +132,7 @@ document.getElementById('answerForm').addEventListener('submit', function (event
     // var form = document.getElementById('answerForm');
     // var currentQuestion = form.getAttribute('data-current-question');
 
-    for (var index = 1; index < 2; index++) {
+    for (var index = 1; index < 5; index++) {
         checkAnswer(document.getElementById('answer' + index).value, index);
         
     }
